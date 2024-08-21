c5a630ec795110d996651456125d6b47e69fa4e3
wenbo <992076760@qq.com>
2024-08-21 15:08:57 +0200

    update log sandbox

--- build/login_to_sandbox.sh
+++ build/login_to_sandbox.sh
@@ -2,20 +2,13 @@ echo "Logging into Sandbox Salesforce Org"
 mkdir -p keys
 
 # Write the key content from the environment variable to the file
-echo "$SANDBOX_CERT_KEY" > keys/server.key
+printf "%s" "$SANDBOX_CERT_KEY" > keys/server.key
 
 # Verify the key file
 if [ ! -s keys/server.key ]; then
   echo "Error: server.key file is empty or not created correctly."
   exit 1
 fi
-
-# Verify the key file
-if [ ! -s keys/server.key ]; then
-  echo "Error: server.key file is empty or not created correctly."
-  exit 1
-fi
-
 
 echo "Authenticating org"
 sfdx force:auth:jwt:grant --clientid $SANDBOX_APP_KEY --jwtkeyfile keys/server.key --username $SANDBOX_USERNAME --setdefaultdevhubusername -a DevHub
