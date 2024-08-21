82deb495188d69506671067473df921cb52ecfc3
wenbo <992076760@qq.com>
2024-08-21 13:06:22 +0200

    update log sanbox

--- build/login_to_sandbox.sh
+++ build/login_to_sandbox.sh
@@ -3,4 +3,4 @@ mkdir keys
 echo $SANDBOX_CERT_KEY | base64 -di > keys/server.key
 
 echo "Authenticating org"
-sfdx force:auth:jwt:grant --clientid $SANDBOX_APP_KEY --jwtkeyfile keys/server.key --username $SANDBOX_USERNAME --setdefaultdevhubusername -a DevHub
+sfdx force:auth:jwt:grant --jwtkeyfile keys/server.key -i $SANDBOX_APP_KEY -u $SANDBOX_USERNAME -d -s -r https://login.salesforce.com
