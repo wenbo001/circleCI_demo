86afb6ecb9065daf9eeb5ce68d21e92e25202e67
wenbo <992076760@qq.com>
2024-08-21 14:55:33 +0200

    update login sandbox

--- build/login_to_sandbox.sh
+++ build/login_to_sandbox.sh
@@ -40,4 +40,4 @@ fi
 
 
 echo "Authenticating org"
-sfdx force:auth:jwt:grant -f keys/server.key -i 3MVG9PwZx9R6_Urd9r8PNJwDnYvA4Bw.SL55ochLfU19uTR9i3aReGiGEnsgcFvq3MHJWqDwZr3jf9mVJYQBm -o liuwenbo@case.com -d -s -r https://login.salesforce.com
+sfdx force:auth:jwt:grant --clientid $SANDBOX_APP_KEY --jwtkeyfile keys/server.key --username $SANDBOX_USERNAME --setdefaultdevhubusername -a DevHub
