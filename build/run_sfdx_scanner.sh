6b9eaf8b85fddb5461d8c2e073126357110fff31
wenbo <992076760@qq.com>
2024-08-22 16:55:37 +0200

    update scanner

--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -1,5 +1,6 @@
 echo "Installing JDK"
-sudo apt-get install openjdk-11-jdk
+sudo apt-get update
+sudo apt-get --fix-missing install default-jdk
 
 echo "Install SFDX Scanner"
 echo -e 'y/n' | sfdx plugins:install @salesforce/sfdx-scanner
