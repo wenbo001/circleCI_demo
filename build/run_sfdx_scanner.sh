8c7cb6bf967a678e7f6bb927a1b7678d32a72300
wenbo <992076760@qq.com>
2024-08-22 16:43:08 +0200

    add apex common

--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -1,5 +1,5 @@
 echo "Installing JDK"
-sudo apt-get install openjdk-8-jdk
+sudo apt-get install openjdk-11-jdk
 
 echo "Install SFDX Scanner"
 echo -e 'y/n' | sfdx plugins:install @salesforce/sfdx-scanner
